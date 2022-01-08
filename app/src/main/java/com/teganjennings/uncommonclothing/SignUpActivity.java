5f2b57994efc379d350ce25c4db64d0f1e84c4b0
teganj <teganjennings1@hotmail.com>
2022-01-08 03:21:59 +0000

    Adding Languages to Signup and main activity

--- app/src/main/java/com/teganjennings/uncommonclothing/SignUpActivity.java
+++ app/src/main/java/com/teganjennings/uncommonclothing/SignUpActivity.java
@@ -1,6 +1,9 @@
 package com.teganjennings.uncommonclothing;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,6 +13,14 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup_activity);
-    }
 
+
+        Button createAccBtn = (Button) findViewById(R.id.signupBtn);
+        createAccBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SignUpActivity.this, MainActivity.class));
+            }
+        });
+    }
 }
